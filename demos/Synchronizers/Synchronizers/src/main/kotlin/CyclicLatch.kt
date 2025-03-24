0835824d0565b0dd3f91d854d823969f3f72f6c4
Paulo Pereira <paulo.pereira@isel.pt>
2025-03-24 18:58:56 +0000

    Adds code written during today's class

--- demos/Synchronizers/Synchronizers/src/main/kotlin/CyclicLatch.kt
+++ demos/Synchronizers/Synchronizers/src/main/kotlin/CyclicLatch.kt
@@ -5,21 +5,30 @@ import kotlin.concurrent.withLock
 
 class CyclicLatch {
 
+    data class Request(var signalled: Boolean = false)
+
     var isOpen = false
+    val requests = mutableListOf<Request>()
 
     private val guard = ReentrantLock()
     private val condition = guard.newCondition()
 
     fun await() {
         guard.withLock {
+            // Check condition
+            if (isOpen)
+                return
+
+            // We will block the current thread
+            val myRequest = Request()
+            requests.add(myRequest)
+
             while (true) {
-
-                // Check condition
-                if (isOpen)
-                    return
-
                 // Wait for it
                 condition.await()
+
+                if (myRequest.signalled)
+                    return
             }
         }
     }
@@ -29,9 +38,10 @@ class CyclicLatch {
             if (!isOpen) {
                 isOpen = true
                 // Notify all waiting threads
+                while (requests.size > 0)
+                    requests.removeFirst().signalled = true
                 condition.signalAll()
 
-                TODO("This implementation is wrong")
                 isOpen = false
             }
         }
