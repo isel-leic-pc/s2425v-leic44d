afb5d11d12ca8a982456aa6c6c6a830a8350955e
Paulo Pereira <paulo.pereira@isel.pt>
2025-04-08 17:34:09 +0100

    Adds code written during today's class

--- demos/Synchronizers/src/main/kotlin/jmm/LazyImpl.kt
+++ demos/Synchronizers/src/main/kotlin/jmm/LazyImpl.kt
@@ -1,12 +1,40 @@
 package isel.leic.pc.demos.jmm
 
-class LazyImpl<out T>(private val initializer: () -> T) : Lazy<T> {
-    override val value: T
-        get() = TODO("Not yet implemented")
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
-    override fun isInitialized(): Boolean {
-        TODO("Not yet implemented")
-    }
+private object UNINITIALIZED_VALUE
+class LazyImpl<out T>(initializer: () -> T) : Lazy<T> {
+
+    private var _initializer: (() -> T)? = initializer
+    @Volatile private var _value: Any? = UNINITIALIZED_VALUE
+    private val guard = ReentrantLock()
+
+    override val value: T
+        get() {
+            if (_value === UNINITIALIZED_VALUE) {
+                guard.withLock {
+                    if (_value === UNINITIALIZED_VALUE) {
+                        _value = _initializer!!()
+                        _initializer = null
+                    }
+                }
+            }
+
+            @Suppress("UNCHECKED_CAST")
+            return _value as T
+        }
+
+    override fun isInitialized(): Boolean = _value !== UNINITIALIZED_VALUE
 }
 
 fun <T> myLazy(initializer: () -> T): Lazy<T> = LazyImpl(initializer)
+
+fun main() {
+
+    val someValue = myLazy {
+        println("Computing value...")
+        null
+    }
+    println("Using someValue = ${someValue.value}")
+}
